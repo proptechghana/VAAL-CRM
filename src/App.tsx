6b02d8500e4a8597cf21fd228e83bf1ebca9c91e
propaibusiness26-jpg <prop.ai.business26@gmail.com>
2026-06-02 06:25:28 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -115,8 +115,8 @@ export default function App() {
            </div>
         </div>
         
-        <main className="flex-1 overflow-y-auto p-4 lg:p-8 custom-scrollbar">
-          <div className="max-w-7xl mx-auto xl:px-4">
+        <main className="flex-1 overflow-y-auto overflow-x-hidden p-4 lg:p-8 custom-scrollbar">
+          <div className="max-w-7xl mx-auto px-0 xl:px-4 w-full">
             <div className="hidden lg:block">
               <TopBar />
             </div>
